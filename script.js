6fc4ab9322f6c92d36a99eafd28022473dba85c0
10994015 <a0938599191@gmail.com>
2021-10-12 04:31:25 +0800

    首頁

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ window.addEventListener("scroll", function(){
     header.classList.toggle('sticky', window.scrollY > 0);
 });
 
+const menuBtn = document.getElementsByClassName('menu-btn')[0];
+const headerNav = document.getElementsByClassName('headerNav')[0];
+const searchBtn = document.querySelector('.searchBtn > i');
+const RWDsearchBox = document.getElementsByClassName('RWDsearchBox')[0];
+menuBtn.addEventListener("click",()=>{
+    menuBtn.classList.toggle('active');
+    headerNav.classList.toggle('active');
+})
+searchBtn.addEventListener("click",()=>{
+    RWDsearchBox.classList.toggle('active');
+})
+
+
+const scrollBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+scrollBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollBtn.addEventListener("click", () => {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+});
+
