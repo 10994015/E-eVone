830d59c0569d81b5ce1ce1763345ee925813cde4
10994015 <a0938599191@gmail.com>
2021-10-13 15:47:00 +0800

    完成

---
+++ PRODUCT_LIST/assets/js/script.js
@@ -0,0 +1,21 @@
+var filter = document.querySelector('.filter i');
+var leftSideBar = document.getElementsByClassName('leftSideBar')[0];
+var barClose = document.querySelector('.barClose i');
+    
+
+filter.addEventListener("click",()=>{
+    leftSideBar.style.display="block";
+    setTimeout(()=>{
+        leftSideBar.style.left="0%";
+        leftSideBar.style.opacity=1;
+    },1)
+})
+barClose.addEventListener("click",()=>{
+        leftSideBar.style.left="-70%";
+        
+        setTimeout(()=>{
+            leftSideBar.style.display="none";
+            leftSideBar.style.opacity=0;
+        },1000)
+        
+})
